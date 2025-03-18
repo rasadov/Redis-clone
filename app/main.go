f42373c35cf871162deee46dc9eb48c56d09a422
rasadov <raufasadov23@gmail.com>
2025-03-18 12:28:07 +0400

    added concurrency

--- app/main.go
+++ app/main.go
@@ -1,40 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
-)
-
-var (
-	conn      net.Conn
-	l         net.Listener
-	err       error
-	message   = make([]byte, 1024)
-	bytesRead int
+	"time"
 )
 
 func main() {
-
-	l, err = net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 	fmt.Println("Listening on port 6379...")
-	conn, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	for {
-		bytesRead, err = conn.Read(message)
-		if err != nil {
-			fmt.Println("Error reading from connection: ", err.Error())
-			break
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err.Error())
+				continue
+			}
+			go handleConnection(ctx, conn)
 		}
-		if bytesRead != 0 {
-			conn.Write([]byte("+PONG\r\n"))
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			time.Sleep(100 * time.Second)
+		}
+	}
+}
+
+func handleConnection(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+	message := make([]byte, 1024)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			bytesRead, err := conn.Read(message)
+			if err != nil {
+				fmt.Println("Error reading from connection: ", err.Error())
+				return
+			}
+			if bytesRead != 0 {
+				conn.Write([]byte("+PONG\r\n"))
+			}
 		}
 	}
 }
